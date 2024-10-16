73768a89143748fed3da4b39824971c412e04c11
Ella Fontenot <ellafont117@gmail.com>
2024-10-15 19:03:40 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -9,6 +9,17 @@
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
+  
+  // COMMENTS FOR HOW TO BUILD COMPUTER GAME
+  // HOW TO DO Rock Paper Scissors:
+  // Give the first user 3 options to pick from: Rock, Paper, or Scissors.
+      //Save this answer
+  //Give the second user the same 3 options to pick from.
+      //Save this answer
+  //Compare the two saved answers against the stored rule conditions
+    //If tie, repeat
+    //If there is a winner announce the user with the winning task.
+
   // return results;
 }
 
