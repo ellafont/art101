7e7b9c9870b750f8bd2ad23a9950ab055635b35b
Ella Fontenot <ellafont117@gmail.com>
2024-10-21 16:31:43 -0700

    lab5 js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5!
+/*
+ * Author: Ella Fontenot <efonteno@ucsc.edu.
+ * Date: Oct. 21st 2024
+*/
 
-// Constants
+// Defining Variables
+make = "Volvo";
+model = "xc70";
+color = "Silver";
+year = 2011;
+
+//calculate age
+age = 2024 - year;
 
 // Functions
-
+//no function in lab 5 rn
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+//function myFunction(param1, param2) {
   // some code here
   // return results;
-}
+//}
 
-function main() {
-  console.log("Main function started.");
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
+
+
+//function main() {
+//  console.log("Main function started.");
   // the code that makes everything happen
-}
+//}
 
 // let's get this party started
-main();
+//main();
+
