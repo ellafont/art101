eabbbe28f679110d871fb1225a096a7c677210ee
Ella Fontenot <ellafont117@gmail.com>
2024-10-28 20:23:18 -0700

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,31 @@
+/**
+ * lab.js - Takes a user's name as input and sorts the letters alphabetically
+ * @author Ella
+ * @date October 24, 2024
+ */
+
+// Function to sort the letters of a name
+function sortUserName() {
+  // Get user input using window.prompt()
+  let userName = window.prompt("Please enter your name :)");
+  
+  // Check if user entered a name
+  if (userName) {
+      // Convert string to array, sort it, and join back to string
+      let nameArray = userName.split("");
+      let sortedArray = nameArray.sort();
+      let sortedName = sortedArray.join("");
+      
+      return sortedName;
+  } else {
+      return "You didn't enter a name!";
+  }
+}
+
+// Output the sorted name to the webpage
+document.writeln("<div class='sorted-name'>", 
+              "Your sorted name is: ",
+              sortUserName(),
+              "</div>");
+
+              
